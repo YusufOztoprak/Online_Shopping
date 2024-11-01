d0e61c9436eaeba62c685497fd918e1d5c3916a3
my265 <my03082004my@gmail.com>
2024-11-01 15:28:11 +0300

    Merge remote-tracking branch 'origin/main'

--- pythonProject5/main.py
+++ pythonProject5/main.py
@@ -1,8 +1,6 @@
 from NewPkg import cart
 from NewPkg import Product
 
-
-
 urun = Product.Product("mehmet", 1,1)
-Product.Product.add_product(urun)
-print(Product.Product.listeyiyazdir())
+urun.add_product(urun)
+urun.listeyiyazdir()
